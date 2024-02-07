6d3f9c266d5d6aab63ababaae10921e12b3ace75
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-02-07 21:18:44 +0900

    BRR: build express web server

---
+++ server.js
@@ -0,0 +1,58 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const res = require("express/Lib/response")
+const app = express();
+const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err){
+       console.log("ERROR", err); 
+    } else {
+        user = JSON.parse(data);
+    }
+})
+
+// 1: Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+// 2 Session code
+// 3 views code 
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4: Routing code
+/* app.get("/", function (req, res) {
+    res.end("HELLO WORLD");
+});
+
+app.get("/gift", function (req, res) {
+    res.end("siz sovg'lar bolimidasiz");
+}); */
+app.post("/create-items", (req, res) => {
+    console.log(req.body);
+    res.json({ test: "success" });
+});
+
+
+
+app.get("/author", (req, res) => {
+    res.render("author", { user: user });
+});
+
+
+
+app.get("/harid", function (req, res) {
+    res.render("harid");
+ 
+});
+
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+    console.log(`The server is running successfelly on port: ${PORT}`);
+});
