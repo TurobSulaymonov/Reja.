492a430203aa9eeddcd3393b7a67979067ac19f7
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-02-17 03:56:44 +0900

    feat: D task

--- train.js
+++ train.js
@@ -1,7 +1,74 @@
+// D task
+
+// Shunday function tuzing, u 2ta string parametr ega bolsin, 
+// hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
+// MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+
+ 
+
+
+
+//define
+
+ function soliution(harf, harf2) {
+  
+     const a = harf.split('').sort().join('');
+       const b = harf2.split('').sort().join('');
+      
+  
+    return a == b;
+  }
+  
+  // call
+  const natija = soliution("mitgroup", "gmtiprou");
+  console.log(natija);  
+  
+
+
+
+
+
+
+
+
+ 
+
+;
+
+
+/* function solution ( my_string, n) {
+    let answer = my_string.slice(0, n);
+  return answer
+}
+console.log(solution('dhkuahos', 3)) */
+
+
+/*  function solution (a, b) {
+    let answer = [];
+    for (let i = a; i <= b; i++) {
+        answer.push([i])
+    }
+
+    return answer
+    
+ }
+
+ console.log(solution(4, 10)); */
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  /* Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
  MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud! */
 // D Task
- class Shop {
+/*  class Shop {
     constructor(non, lagmon, cola) {
         this.non = non;
         this.lagmon = lagmon;
@@ -49,7 +116,7 @@ shop.qoldiq();
 
 shop.sotish('non', 3);
 shop.qabul('cola', 4);
-shop.qoldiq(); 
+shop.qoldiq();  */
 
 
 
