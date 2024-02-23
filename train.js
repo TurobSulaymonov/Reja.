b5cf32e79df8d4960aabd067cd828aa93a7f305b
TeoSulaymonov95 <turobsulaymonov9@gmail.com>
2024-02-24 06:07:42 +0900

    feat: g task yechimi

--- train.js
+++ train.js
@@ -1,24 +1,40 @@
 /* 
+Shunday function tuzingki unga integerlardan iborat array pass bolsin va
+ function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini. */
+
+
+function kattaSonnitopish(arr) {
+  if (arr.length === 0) {
+      return -1; 
+  }
+
+  let maxIndex = 0;
+  let maxValue = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > maxValue) {
+          maxValue = arr[i];
+          maxIndex = i;
+      }
+  }
+
+  return maxIndex;
+}
+
+
+const result = kattaSonnitopish([5, 29, 12, 22, 8]);
+console.log(result);
+
+
+
+
+/* 
 Shunday findDoublers function tuzing, unga faqat bitta string 
 argument pass bolib, agar stringda bir hil harf qatnashgan bolsa true, qatnashmasa false qaytarishi kerak.
 MASALAN: getReverse("hello") return true return qiladi
 
 */
 
-function harfTopish(str) {
-    str = str.toLowerCase();
-  
-    for (let i = 0; i < str.length; i++) {
-      for (let j = i + 1; j < str.length; j++) {
-        if (str[i] === str[j]) {
-          return true;
-        }
-      }
-    }
-  
-    return false;
-  }
-  
-  // call
- const natija = harfTopish("ozbekiston");
-  console.log(natija); 
+/* 
+ */
